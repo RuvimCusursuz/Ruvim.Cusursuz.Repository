7cb2cff380cbfaa12b74d0bc2152b7b9a4951576
RuvimCusursuz <ruvim.cusursuz@gmail.com>
2023-09-06 22:02:50 +0300

    i did a homework Nr 2 for Teckwill

---
+++ src/main/java/homework_nr_2/HomeWorkNr2.java
@@ -0,0 +1,32 @@
+package homework_nr_2;
+
+public class HomeWorkNr2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    //_______________________________________________________
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterlnInAlphabet = 'A';
+        float Pi = 3.114159F;
+
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+        System.out.println(FirstLetterlnInAlphabet);
+        System.out.println(Pi);
+    //_________________________________________________________
+        double Price = 5.65;
+        double Tax = 2.16;
+        int Quantity = 12;
+        double sum = 0;
+
+        System.out.println(Price);
+        System.out.println(Tax);
+        System.out.println(Quantity);
+        System.out.println(sum);
+
+        double total = Price + Tax * Quantity + sum;
+        System.out.println(total);
+
+    }
+}
