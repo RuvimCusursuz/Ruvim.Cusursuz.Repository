1f9a6b165c996de627095813783ad7ae43275a1d
RuvimCusursuz <ruvim.cusursuz@gmail.com>
2023-09-08 20:43:14 +0300

    I created a new Class for working during the lection

---
+++ src/main/java/org/example/MyTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MyTest {
+    public static void main(String[] args) {
+
+        for (int j = 0; j <= 3; j++) {
+            for (int k = 0; k <= j; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
