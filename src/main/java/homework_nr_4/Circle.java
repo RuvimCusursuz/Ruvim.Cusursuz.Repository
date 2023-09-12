66dbc5627e274b1d30bb46c1571e44d8ba36bb86
RuvimCusursuz <ruvim.cusursuz@gmail.com>
2023-09-13 00:43:03 +0300

    Initializing Objekt Circle and TemperatureConverter

---
+++ src/main/java/homework_nr_4/Circle.java
@@ -0,0 +1,16 @@
+package homework_nr_4;
+
+public class Circle {
+     double radius;
+     double pi = 3.14;
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public static void main(String[] args) {
+        Circle circle = new Circle(8.15);
+        circle.calculateArea();
+    }
+    public void calculateArea(){
+        System.out.println(radius * radius * pi );
+    }
+}
